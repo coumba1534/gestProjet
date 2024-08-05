aeda63466df718ae115dab53539a27e56c3deaaf
coumba1534 <bcoumba221@gmail.com>
2024-08-05 21:06:08 +0000

    Ajout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/dashboard-layout/header/header.component';
+import { SidebarComponent } from './shared/dashboard-layout/sidebar/sidebar.component';
+import { FooterComponent } from './shared/dashboard-layout/footer/footer.component';
+import { DashboardLoyoutComponent } from './shared/dashboard-layout/dashboard-loyout/dashboard-loyout.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    DashboardLoyoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/dashboard-layout/dashboard-loyout/dashboard-loyout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-loyout',
+  templateUrl: './dashboard-loyout.component.html',
+  styleUrl: './dashboard-loyout.component.css'
+})
+export class DashboardLoyoutComponent {
+
+}
